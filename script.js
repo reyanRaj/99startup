3d0b622723bfc56ac8e454a5f1d0957230640e2c
reyanRaj <72247352+reyanRaj@users.noreply.github.com>
2021-11-24 17:55:49 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 let topics = ['mobile app','website','product']
+var slideIndex = 1;
 // The index of the currently selected letter from each word
 let index = 0;
 // The index of the currently selected word from topics
 let currentIndex =0;
 // Typing text element
 const typingText = document.querySelector('.typing-text')
+
+//Typing text function
 setTimeout(() => {
     setInterval(() => {
        
@@ -19,3 +22,32 @@ setTimeout(() => {
         typingText.textContent = topics[currentIndex].slice(0,++index)
     }, 200);    
 }, 1000);
+
+//Moving the footer slideshow
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
